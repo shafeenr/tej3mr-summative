cda7e62adbd793e5db89c2abe1693ca880ba6f0d
Shafeen Rahman <shafeenr@tuta.io>
2018-06-03 12:33:17 -0400

    Modified sketch due to hardware constraints

--- traffic.ino
+++ traffic.ino
@@ -1,16 +1,10 @@
-#include <ShiftRegister74HC595.h> // import library to help with controlling shift registers
+#include <ShiftRegister74HC595.h>
 
 #define shiftRegisterAmount 1
 
-// define rather than declare where pins are to save memory
-
-#define serialDataPinOne 4
-#define clockPinOne 6
-#define latchPinOne 5
-
-#define serialDataPinTwo 7
-#define clockPinTwo 8
-#define latchPinTwo 9
+#define serialDataPin 4
+#define clockPin 6
+#define latchPin 5
 
 #define redOne 0
 #define redTwo 1
@@ -23,48 +17,50 @@
 
 #define piezo 2
 
-ShiftRegister74HC595 trafficLight(shiftRegisterAmount, serialDataPinOne, clockPinOne, latchPinOne); // traffic light shift register initialized here
-ShiftRegister74HC595 ped(shiftRegisterAmount + 1, serialDataPinTwo, clockPinTwo, latchPinTwo); // two pedestrian shift registers in series initialized here
+// traffic light timer shift register initialized here
+ShiftRegister74HC595 ped(shiftRegisterAmount, serialDataPin, clockPin, latchPin);
 
 // array containing bit codes to display integers 0-9
 const uint8_t sevenSegmentNumbers[10] = { B1111110, B0110000, B1101101, B1111001, B0110011, B1011111, B1011111, B1110000, B1111111, B1110011 };
 
-void setup() {
-  // turns off all lights before loop function
-  trafficLight.setAllLow();
+void pedTimerCountDown(boolean direction);
+
+void setup() 
+{
 }
 
-void loop() {
-  // signals both directions to stop for two seconds)
-  trafficLight.set(redOne, HIGH);
-  trafficLight.set(redTwo, HIGH);
+void loop() 
+{
+    // signals both directions to stop for two seconds)
+  digitalWrite(redOne, HIGH);
+  digitalWrite(redTwo, HIGH);
   delay(2000);
 
   // signals direction one to proceed, turns on buzzer, calls ped countdown timer function
-  trafficLight.set(redOne, LOW);
-  trafficLight.set(greenOne, HIGH);
+  digitalWrite(redOne, LOW);
+  digitalWrite(greenOne, HIGH);
   tone(piezo, 500);
   pedTimerCountDown(true);
   noTone(piezo); // turns off buzzer
 
   // signals direction one to slow down for three seconds
-  trafficLight.set(greenOne, LOW);
-  trafficLight.set(yellowOne, HIGH);
+  digitalWrite(greenOne, LOW);
+  digitalWrite(yellowOne, HIGH);
   delay(3000);
 
   // both directions told to stop for two seconds (direction two is already red)
-  trafficLight.set(redOne, HIGH);
+  digitalWrite(redOne, HIGH);
   delay(2000);
 
   // signals direction two to proceed
-  trafficLight.set(redTwo, LOW);
-  trafficLight.set(greenTwo, HIGH);
+  digitalWrite(redTwo, LOW);
+  digitalWrite(greenTwo, HIGH);
   tone(piezo, 200);
   pedTimerCountDown(false);
   noTone(piezo);
 
-  trafficLight.set(greenTwo, LOW);
-  trafficLight.set(yellowTwo, HIGH);
+  digitalWrite(greenTwo, LOW);
+  digitalWrite(yellowTwo, HIGH);
   delay(3000);
 }
 
