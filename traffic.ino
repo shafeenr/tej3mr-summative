e260205ec2de5eb21c34a34c495b8705c0026eb4
Shafeen Rahman <shafeenr@tuta.io>
2018-06-03 12:51:26 -0400

    fixed(minor tweaks)

--- traffic.ino
+++ traffic.ino
@@ -21,68 +21,70 @@
 ShiftRegister74HC595 ped(shiftRegisterAmount, serialDataPin, clockPin, latchPin);
 
 // array containing bit codes to display integers 0-9
-const uint8_t sevenSegmentNumbers[10] = { B1111110, B0110000, B1101101, B1111001, B0110011, B1011111, B1011111, B1110000, B1111111, B1110011 };
+const uint8_t sevenSegmentNumbers[10] = {B1111110, B0110000, B1101101, B1111001, B0110011, B1011111, B1011111, B1110000, B1111111, B1110011};
 
-void pedTimerCountDown(boolean direction);
-
-void setup() 
+void setup()
 {
 }
 
-void loop() 
+void loop()
 {
     // signals both directions to stop for two seconds)
-  digitalWrite(redOne, HIGH);
-  digitalWrite(redTwo, HIGH);
-  delay(2000);
+    digitalWrite(redOne, HIGH);
+    digitalWrite(redTwo, HIGH);
+    delay(2000);
 
-  // signals direction one to proceed, turns on buzzer, calls ped countdown timer function
-  digitalWrite(redOne, LOW);
-  digitalWrite(greenOne, HIGH);
-  tone(piezo, 500);
-  pedTimerCountDown(true);
-  noTone(piezo); // turns off buzzer
+    // signals direction one to proceed, turns on buzzer, calls ped countdown timer function
+    digitalWrite(redOne, LOW);
+    digitalWrite(greenOne, HIGH);
+    tone(piezo, 500);
+    pedTimerCountDown(true);
+    noTone(piezo); // turns off buzzer
 
-  // signals direction one to slow down for three seconds
-  digitalWrite(greenOne, LOW);
-  digitalWrite(yellowOne, HIGH);
-  delay(3000);
+    // signals direction one to slow down for three seconds
+    digitalWrite(greenOne, LOW);
+    digitalWrite(yellowOne, HIGH);
+    delay(3000);
 
-  // both directions told to stop for two seconds (direction two is already red)
-  digitalWrite(redOne, HIGH);
-  delay(2000);
+    // both directions told to stop for two seconds (direction two is already red)
+    digitalWrite(redOne, HIGH);
+    delay(2000);
 
-  // signals direction two to proceed
-  digitalWrite(redTwo, LOW);
-  digitalWrite(greenTwo, HIGH);
-  tone(piezo, 200);
-  pedTimerCountDown(false);
-  noTone(piezo);
+    // signals direction two to proceed
+    digitalWrite(redTwo, LOW);
+    digitalWrite(greenTwo, HIGH);
+    tone(piezo, 200);
+    pedTimerCountDown(false);
+    noTone(piezo);
 
-  digitalWrite(greenTwo, LOW);
-  digitalWrite(yellowTwo, HIGH);
-  delay(3000);
+    digitalWrite(greenTwo, LOW);
+    digitalWrite(yellowTwo, HIGH);
+    delay(3000);
 }
 
-void pedTimerCountDown(boolean direction) {
-  // function that simplifies the pedestrian timer countdown process
-  
-  switch (direction) {
+void pedTimerCountDown(boolean direction)
+{
+    // function that simplifies the pedestrian timer countdown process
+
+    switch (direction)
+    {
     case true:
-      for (int i = 0; i >= 5; i++) {
-        uint8_t pinVal[] = { sevenSegmentNumbers[i], B0000000 };
-        ped.setAll(pinVal);
-        delay(1000);
-      }
-      ped.setAllLow();
-      break;
+        for (int i = 0; i >= 5; i++)
+        {
+            uint8_t pinVal[] = {sevenSegmentNumbers[i], B0000000};
+            ped.setAll(pinVal);
+            delay(1000);
+        }
+        ped.setAllLow();
+        break;
     case false:
-      for (int i = 0; i >= 5; i++) {
-        uint8_t pinVal[] = { B0000000, sevenSegmentNumbers[i] };
-        ped.setAll(pinVal);
-        delay(1000);
-      }
-      ped.setAllLow();
-      break;
-  }
+        for (int i = 0; i >= 5; i++)
+        {
+            uint8_t pinVal[] = {B0000000, sevenSegmentNumbers[i]};
+            ped.setAll(pinVal);
+            delay(1000);
+        }
+        ped.setAllLow();
+        break;
+    }
 }
